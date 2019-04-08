c94015cc8d5015166995e4280e42932557087325
Nahla <nahlamoohamed@gmail.com>
2019-04-08 14:31:00 +0200

    validation update

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 var posts = [];
 var totalNumOfPosts = 0;
 
-serviceUrl = 'https://jsonplaceholder.typicode.com/posts';
+serviceUrl = 'http://jsonplaceholder.typicode.com/posts';
 function getPosts(){
   fetch(serviceUrl).then((response) => {
    if(response.ok){
@@ -19,47 +19,57 @@ getPosts();
 
 function getPostbyUserId(){
   var all = document.getElementById('myCheck').checked;
-  console.log(all);
+   var idCheck = validateInput('userId');
+  if(all == true && idCheck){
+    swal({text:"Please select only one option",icon: "warning"});
+  }
   if(all == true){
+    document.getElementById('userId').style = "border-color:#ccc";
     getPosts();
     return;
   }
-  fetch(serviceUrl+`?userId=${document.getElementById('userId').value}`).then((response) => {
-   if(response.ok){
-     return response.json();
-   } 
-    throw new Error('Request failed!');
-  },(networkError) => {
-    console.log(networkError.message);
-  }).then(responseJson => {
-    posts = responseJson;
-    showPosts();
-  });
+ 
+  if(idCheck){
+    fetch(serviceUrl+`?userId=${document.getElementById('userId').value}`).then((response) => {
+     if(response.ok){
+       return response.json();
+     } 
+      throw new Error('Request failed!');
+    },(networkError) => {
+      console.log(networkError.message);
+    }).then(responseJson => {
+      posts = responseJson;
+      showPosts();
+    });
+  }
 }
 function createNewPost(){
-  fetch(serviceUrl, {
-    method: 'POST',
-    body: JSON.stringify({
-      title: document.getElementById('ipTitle').value,
-      body:  document.getElementById('ipBody').value,
-      userId:  document.getElementById('ipUserId').value
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
-  .then(response => {
-    if(response.ok)
-    {
-      console.log("Request done successfully");
-      return response.json();
-    }
-    throw new Error('Post request failed!');
-  },networkError => {
-  console.log(networkError.message);
-  }).then(json => {
-    swal({text:`Post ${json.id} added Successfully!`,icon: "success"});
-  });
+  var t = validateInput('ipTitle') , b = validateInput('ipBody') , id=validateInput('ipUserId');
+  if(t && b && id){
+    fetch(serviceUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        title: document.getElementById('ipTitle').value,
+        body:  document.getElementById('ipBody').value,
+        userId:  document.getElementById('ipUserId').value
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+    .then(response => {
+      if(response.ok)
+      {
+        console.log("Request done successfully");
+        return response.json();
+      }
+      throw new Error('Post request failed!');
+    },networkError => {
+    console.log(networkError.message);
+    }).then(json => {
+      swal({text:`Post ${json.id} added Successfully!`,icon: "success"});
+    });
+  }
 }
 
 function deletePost(postId){
@@ -87,5 +97,17 @@ function showPosts(){
     div.appendChild(header);
     div.appendChild(body);
     postsDiv.appendChild(div);
+  }
+}
+
+function validateInput(element) {
+  var x = document.getElementById(element);
+  if (x.value == "") {
+    x.style = "border-color:red";
+    return false;
+  }
+  else{
+    x.style = "border-color:#ccc";
+    return true;
   }
 }
